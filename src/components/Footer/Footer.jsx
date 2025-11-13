36f686277d99404ced4c8d0cd35df33305fec930
BarrosPL <barros.pedro@academico.ifpb.edu.br>
2025-11-13 08:35:52 -0300

    Footer adicionado.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div></div>
+    <footer className="py-8 border-t border-white">
+        <div className="container mx-auto px-4 text-center text-white">
+          <p className="text-muted-foreground">
+            © 2025 Desenvolvido por Pedro Lucas.
+          </p>
+        </div>
+         <div className='text-lg font-semibold cursor-pointer #111138 justify-center items-center text-center pt-3'>
+                    <span className='text-[#2563EB]'>&lt;</span>
+                    <span className='text-white'>Lucas</span>
+                    <span className='text-[#2563EB]'>/</span>
+                    <span className='text-white'>Barros</span>
+                    <span className='text-[#2563EB]'>&gt;</span>
+         </div>
+      </footer>
   )
 }
 
